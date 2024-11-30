f83fc0b10efeb3d02ceb9b9457fe146655bd30cf
wicaksono21 <131900912+wicaksono21@users.noreply.github.com>
2024-11-30 12:31:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import pytz
 
 # Import configurations
 from stageprompts import INITIAL_ASSISTANT_MESSAGE
-from reviewinstructions import SYSTEM_INSTRUCTIONS, REVIEW_INSTRUCTIONS
+from reviewinstructions import SYSTEM_INSTRUCTIONS, REVIEW_INSTRUCTIONS, GRADING_CRITERIA, SCORE_DISCLAIMER
+
 # Initialize Firebase
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["FIREBASE"]))
